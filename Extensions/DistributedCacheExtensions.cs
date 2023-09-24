c5cdf086c6eb19b8bd8877fedb23070a424495af
furkan-developer <furknn2112@gmail.com>
2023-09-24 11:34:46 +0300

    IDistributedCache extension

---
+++ Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Caching.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        private static JsonSerializerOptions _serializeOptions = new JsonSerializerOptions()
+        {
+            PropertyNamingPolicy = null,
+            WriteIndented = true,
+            AllowTrailingCommas = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+        public static Task SetAsync<T>(this IDistributedCache cache, string key, T value)
+        {
+            return SetAsync(cache, key, value, new DistributedCacheEntryOptions());
+        }
+        public static Task SetAsync<T>(this IDistributedCache cache, string key, T value, DistributedCacheEntryOptions options)
+        {
+            var bytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(value, _serializeOptions));
+            return cache.SetAsync(key, bytes, options);
+        }
+        public static bool TryGetValue<T>(this IDistributedCache cache, string key, out T? value)
+        {
+            var val = cache.Get(key);
+            value = default;
+            if (val == null) return false;
+            value = JsonSerializer.Deserialize<T>(val, _serializeOptions);
+            return true;
+        }
+    }
+}
